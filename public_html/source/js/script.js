3722a2a8a24b4c73b0d88bd290e8f13dcb732ff2
handokoadjipangestu <handokoadjipangestu@gmail.com>
2022-02-12 11:46:14 +0700

    first commit

---
+++ public_html/source/js/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+  $('#products').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    margin: 50,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 3
+      }
+    }
+  })
+
+  var owl = $('#products');
+  owl.owlCarousel();
+  $('.customNextBtn').click(function() {
+    owl.trigger('next.owl.carousel');
+  })
+  $('.customPrevBtn').click(function() {
+    owl.trigger('prev.owl.carousel', [300]);
+  })
+  $(".customNextBtn, .customPrevBtn").hover(
+    function() {
+      owl.trigger('stop.owl.autoplay')
+    },
+    function(e) {
+      owl.trigger('play.owl.autoplay')
+    }
+  );
+});
